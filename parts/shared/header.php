36ddd0e16f1220b1265caddc4b02582637929c0a
Andy Marciniak <andy@aardvarkmedia.co.uk>
2012-11-29 10:34:21 +0000

    Added logo to header

--- parts/shared/header.php
+++ parts/shared/header.php
@@ -1,3 +1,4 @@
-<header class="row main">										
+<header class="row main">
+	<h2 class="logo"><a href="/"><?php bloginfo( 'name' ); ?></a></h2>									
 	<?php wp_nav_menu( array( 'theme_location' => 'primary', 'container' => 'nav' ) ); ?>
 </header>
